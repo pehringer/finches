d1fa907a795da41e077ab136919f3a0672981b83
pehringer <jacob.ray.pehringer@gmail.com>
2025-04-08 22:46:31 -0600

    basic vm api

--- main.go
+++ main.go
@@ -4,6 +4,23 @@ import (
 	"fmt"
 )
 
+type machine struct {
+	stack []float32
+	flags byte
+}
+
+func setInputs(inputs []float32) *machine {
+	return nil
+}
+
+func (m *machine) execute(instruction [5]byte) error {
+	return nil
+}
+
+func (m *machine) getOutputs() []float32 {
+	return nil
+}
+
 func main() {
 	fmt.Println("fungen")
 }
