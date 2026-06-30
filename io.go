421b248039d29c6d121503953244a0c8d99b411d
pehringer <jacob.ray.pehringer@gmail.com>
2026-06-30 16:44:53 -0600

    Better code generation.

--- io.go
+++ io.go
@@ -59,129 +59,123 @@ func read(filename string) ([][]float64, []float64, error) {
 	return inputs, outputs, nil
 }
 
+const programFormat = `
+package main
 
-var registerVariable map[int]string = map[int]string{
-	0:  "A",
-	1:  "B",
-	2:  "C",
-	3:  "D",
-	4:  "E",
-	5:  "F",
-	6:  "G",
-	7:  "H",
-	8:  "I",
-	9:  "J",
-	10: "K",
-	11: "L",
-	12: "M",
-	13: "N",
-	14: "O",
-	15: "P",
-}
-
-var instructionArguments map[uint16]int = map[uint16]int{
-	opcodeAD: 3,
-	opcodeSB: 3,
-	opcodeML: 3,
-	opcodeDV: 3,
-	opcodePW: 3,
-	opcodeSQ: 2,
-	opcodeEX: 2,
-	opcodeLG: 2,
-	opcodeSN: 2,
-	opcodeAS: 2,
-	opcodeCS: 2,
-	opcodeAC: 2,
-	opcodeMN: 3,
-	opcodeMX: 3,
-	opcodeLT: 3,
-	opcodeGT: 3,
-}
-
-var instructionFormat map[uint16]string = map[uint16]string{
-	opcodeAD: "\t\t%s = %s + %s\n",
-	opcodeSB: "\t\t%s = %s - %s\n",
-	opcodeML: "\t\t%s = %s * %s\n",
-	opcodeDV: "\t\t%s = divide(%s, %s)\n",
-	opcodePW: "\t\t%s = math.Pow(%s, %s)\n",
-	opcodeSQ: "\t\t%s = math.Sqrt(%s)\n",
-	opcodeEX: "\t\t%s = math.Exp(%s)\n",
-	opcodeLG: "\t\t%s = math.Log(%s)\n",
-	opcodeSN: "\t\t%s = math.Sin(%s)\n",
-	opcodeAS: "\t\t%s = math.Asin(%s)\n",
-	opcodeCS: "\t\t%s = math.Cos(%s)\n",
-	opcodeAC: "\t\t%s = math.Acos(%s)\n",
-	opcodeMN: "\t\t%s = math.Min(%s, %s)\n",
-	opcodeMX: "\t\t%s = math.Max(%s, %s)\n",
-	opcodeLT: "\t\t%s = float(%s < %s)\n",
-	opcodeGT: "\t\t%s = float(%s > %s)\n",
-}
-
-const programFormat = `package main
 import "os"
 import "fmt"
 import "math"
 import "strconv"
 
 func float(b bool) float64 {
-	if b {
-		return 1
-	}
-	return 0
+  if b {
+    return 1
+  }
+  return 0
 }
 
 func divide(n, d float64) float64 {
-	if math.Abs(d) > 1e-9 {
-		return n / d
-	}
-	if math.Abs(n) < 1e-9 {
-		return math.NaN()
-	}
-	if n > 0 {
-		return math.Inf(1)
-	}
-	return math.Inf(-1)
+  if math.Abs(d) > 1e-9 {
+    return n / d
+  }
+  if math.Abs(n) < 1e-9 {
+    return math.NaN()
+  }
+  if n > 0 {
+    return math.Inf(1)
+  }
+  return math.Inf(-1)
 }
 
-%s
-func main() {
-	for i := 1; i < len(os.Args); i += %s {
+var r = [16]float64{
+  %f,
+  %f,
+  %f,
+  %f,
+  %f,
+  %f,
+  %f,
+  %f,
+  %f,
+  %f,
+  %f,
+  %f,
+  %f,
+  %f,
+  %f,
+  %f,
+}
 
-%s
-%s
-	}
-	fmt.Println(P)
+func main() {
+  for i := 1; i < len(os.Args); i += %d {
+%s  }
+  fmt.Println(r[15])
 }`
 
 func write(path string, inputs int, constants []float64, instructions []uint16) error {
-	variables := ""
-	for i := range constants {
-		name := registerVariable[i]
-		value := constants[i]
-		variables += fmt.Sprintf("var %s float64 = %f\n", name, value)
-	}
-	increment := fmt.Sprintf("%d", inputs)
-	arguments := ""
-	for i := range inputs {
-		name := registerVariable[i]
-		arguments += fmt.Sprintf("\t\t%s, _ = strconv.ParseFloat(os.Args[i+%d], 64)\n", name, i)
-	}
 	code := ""
+	for i := range inputs {
+		code += fmt.Sprintf("    r[%d], _ = strconv.ParseFloat(os.Args[i+%d], 64)\n", i, i)
+	}
 	for i := range instructions {
-		argument1 := registerVariable[int(instructions[i] >> resultShift & shiftMask)]
-		argument2 := registerVariable[int(instructions[i] >> firstShift & shiftMask)]
-		argument3 := registerVariable[int(instructions[i] >> secondShift & shiftMask)]
-		opcode := instructions[i] & opcodeMask
-		switch instructionArguments[opcode] {
-		case 1:
-			code += fmt.Sprintf(instructionFormat[opcode], argument1)
-		case 2:
-			code += fmt.Sprintf(instructionFormat[opcode], argument1, argument2)
-		case 3:
-			code += fmt.Sprintf(instructionFormat[opcode], argument1, argument2, argument3)
+		result := fmt.Sprintf("r[%d]", instructions[i] >> resultShift & shiftMask)
+		first := fmt.Sprintf("r[%d]", instructions[i] >> firstShift & shiftMask)
+		second := fmt.Sprintf("r[%d]", instructions[i] >> secondShift & shiftMask)
+		switch instructions[i] & opcodeMask {
+		case opcodeAD:
+			code += fmt.Sprintf("    %s = %s + %s\n", result, first, second)
+		case opcodeSB:
+			code += fmt.Sprintf("    %s = %s - %s\n", result, first, second)
+		case opcodeML:
+			code += fmt.Sprintf("    %s = %s * %s\n", result, first, second)
+		case opcodeDV:
+			code += fmt.Sprintf("    %s = divide(%s, %s)\n", result, first, second)
+		case opcodePW:
+			code += fmt.Sprintf("    %s = math.Pow(%s, %s)\n", result, first, second)
+		case opcodeSQ:
+			code += fmt.Sprintf("    %s = math.Sqrt(%s)\n",  result, first)
+		case opcodeEX:
+			code += fmt.Sprintf("    %s = math.Exp(%s)\n", result, first)
+		case opcodeLG:
+			code += fmt.Sprintf("    %s = math.Log(%s)\n", result, first)
+		case opcodeSN:
+			code += fmt.Sprintf("    %s = math.Sin(%s)\n", result, first)
+		case opcodeAS:
+			code += fmt.Sprintf("    %s = math.Asin(%s)\n", result, first)
+		case opcodeCS:
+			code += fmt.Sprintf("    %s = math.Cos(%s)\n", result, first)
+		case opcodeAC:
+			code += fmt.Sprintf("    %s = math.Acos(%s)\n", result, first)
+		case opcodeMN:
+			code += fmt.Sprintf("    %s = math.Min(%s, %s)\n", result, first, second)
+		case opcodeMX:
+			code += fmt.Sprintf("    %s = math.Max(%s, %s)\n", result, first, second)
+		case opcodeLT:
+			code += fmt.Sprintf("    %s = float(%s < %s)\n", result, first, second)
+		case opcodeGT:
+			code += fmt.Sprintf("    %s = float(%s > %s)\n", result, first, second)
 		}
 	}
-	program := fmt.Sprintf(programFormat, variables, increment, arguments, code)
+	program := fmt.Sprintf(programFormat,
+		constants[0],
+		constants[1],
+		constants[2],
+		constants[3],
+		constants[4],
+		constants[5],
+		constants[6],
+		constants[7],
+		constants[8],
+		constants[9],
+		constants[10],
+		constants[11],
+		constants[12],
+		constants[13],
+		constants[14],
+		constants[15],
+		inputs,
+		code,
+	)
 	err := os.WriteFile(path, []byte(program), 0644)
 	if err != nil {
 		return fmt.Errorf("could not write file: %w", err)
